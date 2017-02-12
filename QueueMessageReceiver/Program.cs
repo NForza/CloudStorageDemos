dc126d8acb636012e39b20bdf5f56479047d9e61
ronaldharmsen <ronaldharmsen@RONALDHARMSC116>
2017-02-12 20:16:03 +0100

    Add project files.

---
+++ QueueMessageReceiver/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueMessageReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cloudConnectionString = ConfigurationManager.ConnectionStrings["CloudStorage"].ConnectionString;
+            var account = CloudStorageAccount.Parse(cloudConnectionString);
+            var client = account.CreateCloudQueueClient();
+            var queue = client.GetQueueReference("queue");
+            queue.CreateIfNotExists();
+
+            var nextMessage = queue.GetMessage(TimeSpan.FromSeconds(10));
+            while (true)
+            {               
+                if (nextMessage==null)
+                    Console.WriteLine("No message in q");
+                else
+                    Console.WriteLine($"Data from q: {nextMessage.AsString}");
+
+                nextMessage = queue.GetMessage(TimeSpan.FromSeconds(10));
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ QueueMessageSender/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueMessageSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cloudConnectionString = ConfigurationManager.ConnectionStrings["CloudStorage"].ConnectionString;
+            var account = CloudStorageAccount.Parse(cloudConnectionString);
+            var client = account.CreateCloudQueueClient();
+            var queue = client.GetQueueReference("queue");
+            queue.CreateIfNotExists();
+
+            var msg = new CloudQueueMessage("Hello");
+            queue.AddMessage(msg);
+        }
+    }
+}
